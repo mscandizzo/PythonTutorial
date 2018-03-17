4bb9e8aa6a9b6e8cab4141e3ead6af6256adc0fa
spartners <mariano.scandizzo@spartners.me>
2018-03-17 00:07:19 -0400

    more cleaning

--- ch6/oop/class.Introduction.py
+++ ch6/oop/class.Introduction.py
@@ -94,6 +94,27 @@ print(d.__class__.mro())  # notice another way to get the MRO
 #  <class '__main__.C'>, <class '__main__.A'>, <class 'object'>]
 
 """
+INIT
+"""
+
+
+class Rectangle():
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+        self.side_b = side_b
+
+    def area(self):
+        return self.side_a * self.side_b
+
+
+r1 = Rectangle(10, 4)
+print(r1.side_a, r1.side_b)  # 10 4
+print(r1.area())  # 40
+
+r2 = Rectangle(7, 3)
+print(r2.area())  # 21
+
+"""
 MUST NOT DO !!
 NEVER DUPLICATE ATTRIBUTES!!
 """
@@ -114,3 +135,20 @@ class Ebook(Book):
         self.publisher = publisher
         self.pages = pages
         self.format_ = format_
+
+"""
+CLASSES ARE BLUE PRINTS
+ONCE CREATED THEY WILL MULTIPLY AND MUTATE
+"""
+
+
+class Price():
+    def final_price(self, vat, discount=0):
+        """Returns price after applying vat and fixed discount."""
+        return (self.net_price * (100 + vat) / 100) - discount
+
+
+p1 = Price()
+p1.net_price = 100
+print(Price.final_price(p1, 20, 10))  # 110 (100 * 1.2 - 10)
+print(p1.final_price(20, 10))  # equivalent
